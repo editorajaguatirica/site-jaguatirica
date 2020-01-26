cd1c5f7876278d7cd7466ac0a4639cffc09fd366
editorajaguatirica <jaguatiricadigital@gmail.com>
2020-01-26 09:15:56 -0300

    Update whatsapp.js

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,8 +1,8 @@
 (function () {
     var options = {
         whatsapp: "+5521999347638", // WhatsApp number
-        call_to_action: "Tire suas dúvidas pelo Whatsapp. Fale conosco agora.", // Call to action
-        position: "right", // Position may be 'right' or 'left'
+        call_to_action: "Tire suas dúvidas pelo Whatsapp.", // Call to action
+        position: "left", // Position may be 'right' or 'left'
     };
     var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
     var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
