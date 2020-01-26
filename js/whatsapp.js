d166718ed1e7388487accb3aa4d40d04eaf3b9d1
editorajaguatirica <jaguatiricadigital@gmail.com>
2020-01-26 09:03:12 -0300

    Atualização widget whatsapp

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,15 +1,11 @@
-<script type="text/javascript">
 (function () {
-var options = {
-whatsapp: "+551999347638", // Número do WhatsApp
-company_logo_url: "//publique.editorajaguatirica.com.br/img/logo.png", // URL com o logo da empresa
-greeting_message: "Olá! Somos a Editora Jaguatirica. Em que podemos ajudar ?.", // Texto principal
-call_to_action: "Transforme suas ideias em livros. Envie-nos seu original para análise.", // Chamada para ação
-position: "right", // Posição do widget na página 'right' ou 'left'
-};
-var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    var options = {
+        whatsapp: "+5521999347638", // WhatsApp number
+        call_to_action: "Tire suas dúvidas pelo Whatsapp. Fale conosco agora.", // Call to action
+        position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
 })();
-</script>
